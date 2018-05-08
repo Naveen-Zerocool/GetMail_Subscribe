4707353adcb360a8fec318d1cd793604d50f4d34
Naveen Boobalakrishnan <ilovemybusiness.square@gmail.com>
2018-05-08 18:55:34 +0530

    Changes made for working in heroku

--- MailSub/settings.py
+++ MailSub/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0kjr0^@ydz&k5fbe4g400watx71bp@q_y79yv&$d%7!_0@x0yx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://dailymailsubscription.herokuapp.com/']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
---
+++ README.md
@@ -0,0 +1,5 @@
+This is a django application
+
+clone the repo
+install dependency
+run the application
